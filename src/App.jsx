095aa7b07eb447fc6dab1d7e7034e256e7ef1b17
rohanrit <rohanrit@gmail.com>
2024-04-19 23:34:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import JobsPage from './pages/JobsPage';
+import MainLayout from './layouts/MainLayout';
+import NotFoundPage from './pages/NotFoundPage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
+      <Route path='*' element={<NotFoundPage />} />
+    </Route>
+  )  
+  // createRoutesFromElements(<Route index element={<HomePage/>} />)  
+);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,13 @@
+import JobListings from "../components/JobListings";
+import ViewAllJobs from "../components/ViewAllJobs";
+
+const JobsPage = () => {
+  return (
+    <>
+      <JobListings />
+      <ViewAllJobs />
+    </>
+  );
+};
+
+export default JobsPage;
